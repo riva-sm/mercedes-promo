97d4373eaa37b3e2684834f3289da36669134e9e
Xenia Filippova <32355526+riva-sm@users.noreply.github.com>
2021-09-22 00:31:51 +0800

    день 1, аккордеон

---
+++ js/accordion.js
@@ -0,0 +1,19 @@
+const btns = document.querySelectorAll(".feature__link");
+const lists = document.querySelectorAll(".feature-sub");
+
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener("click", () => console.log(btns[i]));
+// }
+
+btns.forEach((btnItem, index) => {
+  btnItem.addEventListener("click", () => {
+    btns.forEach((btnItem) => {
+      btnItem.classList.remove("feature__link_active");
+    });
+    btnItem.classList.add("feature__link_active");
+    lists.forEach((listItem) => {
+      listItem.classList.add("hidden");
+    });
+    lists[index].classList.remove("hidden");
+  });
+});
